ddf2d855925b246d193f2147b987573a5a819c2d
chaplianski <chaplianski@gmail.com>
2021-10-06 22:03:20 +0300

    Add variant 8 (work)

--- app/src/main/java/com/example/thecatapi/MainActivity.kt
+++ app/src/main/java/com/example/thecatapi/MainActivity.kt
@@ -75,38 +75,4 @@ class MainActivity : AppCompatActivity() {
                 MainViewModelFactory(APIService.getApiService())
             )[MainViewModel::class.java]
     }
-
-/*
-    fun FlipListener(front: View, back: View) {
-        this.mFrontView = front
-        this.mBackView = back
-        this.mBackView.setVisibility(View.GONE)
-    }
-
-    fun onAnimationUpdate(animation: ValueAnimator) {
-        val value = animation.animatedFraction
-        val scaleValue = 0.625f + 1.5f * (value - 0.5f) * (value - 0.5f)
-        if (value <= 0.5f) {
-            this.mFrontView.setRotationY(180 * value)
-            this.mFrontView.setScaleX(scaleValue)
-            this.mFrontView.setScaleY(scaleValue)
-            if (mFlipped) {
-                setStateFlipped(false)
-            }
-        } else {
-            this.mBackView.setRotationY(-180 * (1f - value))
-            this.mBackView.setScaleX(scaleValue)
-            this.mBackView.setScaleY(scaleValue)
-            if (!mFlipped) {
-                setStateFlipped(true)
-            }
-        }
-    }
-
-    private fun setStateFlipped(flipped: Boolean) {
-        mFlipped = flipped
-        this.mFrontView.setVisibility(if (flipped) View.GONE else View.VISIBLE)
-        this.mBackView.setVisibility(if (flipped) View.VISIBLE else View.GONE)
-    }
-}*/
 }
